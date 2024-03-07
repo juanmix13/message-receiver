b9dceaecf3b3cff5a7ecb1ac7bb24d60066f5b1e
Juan Miguel Duran <48862245+juanmix13@users.noreply.github.com>
2024-03-07 13:52:21 +0100

    Files from Spring Initializer
    Files from Spring Initializer

---
+++ src/test/java/com/learning/messagereceiver/MessageReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.messagereceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
